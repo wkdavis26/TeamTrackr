79c3c80a13069b61ad0c745859e4f9ada4315c7e
wkdavis26 <wkdavis26@gmail.com>
2026-03-03 17:53:12 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -74,7 +74,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <div className="max-w-6xl mx-auto px-4 py-8">
+      <div className="max-w-7xl mx-auto px-4 py-8">
         {/* Header */}
         <motion.header
           initial={{ opacity: 0, y: -20 }}
