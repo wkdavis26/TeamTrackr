4aa2d842f95fc4dd64701ed4bcdd47fc337a839d
wkdavis26 <wkdavis26@gmail.com>
2026-03-06 16:27:12 +0000

    File changes

--- src/components/sports/sportsApi.jsx
+++ src/components/sports/sportsApi.jsx
@@ -389,8 +389,8 @@ const parseESPNEvent = (event, league, favoriteTeamIds) => {
     homeId = mapBundesligaTeamToId(homeTeam.team?.abbreviation || '');
     awayId = mapBundesligaTeamToId(awayTeam.team?.abbreviation || '');
   } else if (league === 'MLS') {
-    homeId = mapMLSTeamToId(homeTeam.team?.displayName || '');
-    awayId = mapMLSTeamToId(awayTeam.team?.displayName || '');
+    homeId = mapMLSTeamToId(homeTeam.team?.abbreviation || '');
+    awayId = mapMLSTeamToId(awayTeam.team?.abbreviation || '');
   } else if (league === 'NCAAB') {
     homeId = `ncaab-${homeTeam.team?.abbreviation?.toLowerCase()}`;
     awayId = `ncaab-${awayTeam.team?.abbreviation?.toLowerCase()}`;
