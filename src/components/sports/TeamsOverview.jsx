410efe42fd2008e91f779353f24ef6600252c2b1
wkdavis26 <wkdavis26@gmail.com>
2026-03-06 17:14:39 +0000

    File changes

--- src/components/sports/TeamsOverview.jsx
+++ src/components/sports/TeamsOverview.jsx
@@ -519,6 +519,9 @@ export default function TeamsOverview({ favoriteTeams }) {
                       </div>
                       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         {teams.map(team => (
+                          league === 'F1' ? (
+                            <F1StandingCard key={team.team_id} team={team} />
+                          ) : (
                           <TeamStandingCard
                            key={team.team_id}
                            team={team}
@@ -527,6 +530,7 @@ export default function TeamsOverview({ favoriteTeams }) {
                            resolvedColor={teamColors[team.team_id]}
                            apRankings={apRankings}
                           />
+                          )
                         ))}
                       </div>
                     </div>
