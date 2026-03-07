3454d84ca8748a5026e855a03435cf8a3262a91d
wkdavis26 <wkdavis26@gmail.com>
2026-03-07 17:50:45 +0000

    File changes

--- src/components/sports/sportsApi.jsx
+++ src/components/sports/sportsApi.jsx
@@ -635,7 +635,9 @@ export const fetchAllSchedules = async (favoriteTeams) => {
   const liveWindowStart = new Date(now.getTime() - 4 * 60 * 60 * 1000);
 
   // Fetch all schedules in parallel
-  const [nflGames, nhlGames, mlbGames, nbaGames, wnbaGames, plGames, laligaGames, mlsGames, f1Games, ncaafGames, worldCupGames, euroGames, intlGames, serieAGames, bundesligaGames, ncaabGames, ncaaBaseballGames, womensIntlGames] = await Promise.all([
+  const hasEuropeanLeague = ['Premier League', 'La Liga', 'Serie A', 'Bundesliga'].some(l => teamIdsByLeague[l]);
+
+  const [nflGames, nhlGames, mlbGames, nbaGames, wnbaGames, plGames, laligaGames, mlsGames, f1Games, ncaafGames, worldCupGames, euroGames, intlGames, serieAGames, bundesligaGames, ncaabGames, ncaaBaseballGames, womensIntlGames, uclGames] = await Promise.all([
     teamIdsByLeague['NFL'] ? fetchNFLSchedule() : Promise.resolve([]),
     teamIdsByLeague['NHL'] ? fetchNHLSchedule() : Promise.resolve([]),
     teamIdsByLeague['MLB'] ? fetchMLBSchedule() : Promise.resolve([]),
@@ -654,6 +656,7 @@ export const fetchAllSchedules = async (favoriteTeams) => {
     teamIdsByLeague['Bundesliga'] ? fetchBundesligaSchedule() : Promise.resolve([]),
     teamIdsByLeague['NCAAB'] ? fetchNCAABSchedule() : Promise.resolve([]),
     teamIdsByLeague['NCAAB-Baseball'] ? fetchNCAABaseballSchedule() : Promise.resolve([]),
+    hasEuropeanLeague ? fetchChampionsLeagueSchedule() : Promise.resolve([]),
   ]);
 
       // Parse NCAAF games
