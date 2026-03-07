75f5f71ecb7beb551a7aa4ce03d087a0280a7c54
wkdavis26 <wkdavis26@gmail.com>
2026-03-07 17:52:34 +0000

    File changes

--- src/components/sports/LeagueGames.jsx
+++ src/components/sports/LeagueGames.jsx
@@ -256,7 +256,7 @@ const SPORT_GROUPS = [
   { label: '🏀 Basketball', keys: ['NBA', 'WNBA', 'NCAAB'] },
   { label: '⚾ Baseball', keys: ['MLB', 'NCAAB-Baseball'] },
   { label: '🏒 Hockey', keys: ['NHL'] },
-  { label: '⚽ Soccer', keys: ['Premier League', 'La Liga', 'Serie A', 'Bundesliga', 'MLS', 'International Football'] },
+  { label: '⚽ Soccer', keys: ['Premier League', 'La Liga', 'Serie A', 'Bundesliga', 'Champions League', 'MLS', 'International Football'] },
   { label: '🏎️ Motorsport', keys: ['F1'] },
 ];
 
