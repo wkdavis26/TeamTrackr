70b4fc41ddcd2ffe7d251d93befe6365925b0f55
wkdavis26 <wkdavis26@gmail.com>
2026-03-10 15:08:41 +0000

    File changes

--- functions/f1Standings.ts
+++ functions/f1Standings.ts
@@ -76,7 +76,7 @@ Deno.serve(async (req) => {
       pts: String(entry.points ?? 0),
     }));
 
-    return Response.json({ drivers, constructors, _year: year, _driverCount: drivers.length });
+    return Response.json({ _year: year, _driverCount: drivers.length, first: drivers[0] });
   } catch (error) {
     return Response.json({ error: error.message }, { status: 500 });
   }
