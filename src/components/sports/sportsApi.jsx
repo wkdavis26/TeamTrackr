bc5f9c0631899d2a974f9c4ac855b69fcf35c7b3
wkdavis26 <wkdavis26@gmail.com>
2026-03-10 17:43:22 +0000

    File changes

--- src/components/sports/sportsApi.jsx
+++ src/components/sports/sportsApi.jsx
@@ -913,7 +913,7 @@ export const fetchAllSchedules = async (favoriteTeams) => {
       const favoriteTeamId = teamIdsByLeague['La Liga'].find(id => id === homeId || id === awayId);
       if (!favoriteTeamId) return;
       const gameDate = typeof game.date === 'string' ? new Date(game.date) : game.date;
-      if (gameDate <= now) return;
+      if (gameDate < now) return;
       allGames.push({
         id: `ll-${game.id}`,
         date: gameDate,
