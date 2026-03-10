3bc36f5cbc08ae3356ef56bb27f9299b3535f8ab
wkdavis26 <wkdavis26@gmail.com>
2026-03-10 22:02:25 +0000

    File changes

--- src/components/sports/sportsApi.jsx
+++ src/components/sports/sportsApi.jsx
@@ -933,8 +933,8 @@ export const fetchAllSchedules = async (favoriteTeams) => {
     const laligaIds = teamIdsByLeague['La Liga'];
     console.log('[La Liga] Looking for:', laligaIds, 'Sample game:', laligaGames[0]);
     laligaGames.forEach(game => {
-      const homeId = `la-liga-${game.homeTeam?.id}`;
-      const awayId = `la-liga-${game.awayTeam?.id}`;
+      const homeId = game.homeTeam?.id;
+      const awayId = game.awayTeam?.id;
       console.log('[La Liga] Game IDs:', homeId, awayId);
       const favoriteTeamId = laligaIds.find(id => id === homeId || id === awayId);
       if (!favoriteTeamId) return;
