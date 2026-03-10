03d83de334a2e2022937f0548b9c98913179dfff
wkdavis26 <wkdavis26@gmail.com>
2026-03-10 15:29:50 +0000

    File changes

--- functions/f1Standings.ts
+++ functions/f1Standings.ts
@@ -31,7 +31,7 @@ const DRIVER_FLAG = {
   'PIA': 'au', 'RUS': 'gb', 'ALO': 'es', 'STR': 'ca', 'PER': 'mx',
   'GAS': 'fr', 'OCO': 'fr', 'TSU': 'jp', 'ALB': 'gb', 'HUL': 'de',
   'MAG': 'dk', 'BOT': 'fi', 'ZHO': 'cn', 'LAW': 'nz', 'BEA': 'gb',
-  'DOO': 'au', 'ANT': 'it', 'HAD': 'us', 'BOR': 'br', 'COL': 'ar', 'LIN': 'gb',
+  'DOO': 'au', 'ANT': 'it', 'HAD': 'fr', 'BOR': 'br', 'COL': 'ar', 'LIN': 'se',
 };
 
 Deno.serve(async (req) => {
