94bbecbc34e16bfd0d8036cbc96d14fb057ac40c
wkdavis26 <wkdavis26@gmail.com>
2026-03-10 17:50:40 +0000

    File changes

--- functions/mlsTeams.ts
+++ functions/mlsTeams.ts
@@ -32,11 +32,10 @@ Deno.serve(async (req) => {
     }
 
     const teams = data.response.map(item => {
-      const teamName = item.team.name.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '');
       return {
-        id: `mls-${teamName}`,
+        id: `mls-${item.team.id}`,
         name: item.team.name,
-        abbreviation: teamName,
+        abbreviation: item.team.id,
         logo: item.team.logo || null,
       };
     });
