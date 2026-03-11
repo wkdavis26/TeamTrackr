7989e48136ac301ec7c2290303ece0542d04c23b
wkdavis26 <wkdavis26@gmail.com>
2026-03-11 15:03:07 +0000

    File changes

--- src/components/sports/sportsApi.jsx
+++ src/components/sports/sportsApi.jsx
@@ -629,11 +629,7 @@ const getTeamEmoji = (teamId) => {
 
 // Main function to fetch all schedules for favorite teams
 export const fetchAllSchedules = async (favoriteTeams) => {
-  console.log('[fetchAllSchedules] Called with favoriteTeams:', favoriteTeams);
-  if (!favoriteTeams || favoriteTeams.length === 0) {
-    console.log('[fetchAllSchedules] No favorite teams, returning empty');
-    return [];
-  }
+  if (!favoriteTeams || favoriteTeams.length === 0) return [];
   
   const teamIdsByLeague = {};
   favoriteTeams.forEach(team => {
